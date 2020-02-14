d74c87a08541b9138710516d4c5b60c5cd0a6707
morykeita <morykeita@protonmail.com>
2020-02-14 00:14:29 -0500

    refactoring

--- user-service/src/main/java/com/morykeita/userservice/controller/UserController.java
+++ user-service/src/main/java/com/morykeita/userservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.morykeita.userservice.controller;
 
 import com.morykeita.userservice.dto.UserDto;
 import com.morykeita.userservice.model.CreateUserRequest;
+import com.morykeita.userservice.model.CreatedUserResponse;
 import com.morykeita.userservice.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -16,19 +17,26 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
     private UserService userService;
+
+    @Autowired
+    public  UserController(UserService userService){
+        this.userService = userService;
+    }
+
     @GetMapping("/status/check")
     public String test(){
         return "account-service working";
     }
 
-    //@PostMapping()
-    public ResponseEntity createUser(@Valid  @RequestBody CreateUserRequest userDetails){
+    @PostMapping
+    public ResponseEntity<CreatedUserResponse> createUser(@Valid  @RequestBody CreateUserRequest userDetails){
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserDto userDto = mapper.map(userDetails,UserDto.class);
-        userService.createUser(userDto);
-        return  new ResponseEntity(HttpStatus.CREATED);
+        UserDto createdUser = userService.createUser(userDto);
+        CreatedUserResponse responseBody = mapper.map(createdUser,CreatedUserResponse.class);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+
     }
 }
